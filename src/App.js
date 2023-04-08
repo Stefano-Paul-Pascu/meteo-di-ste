fc07656368c5be38c741392b46d724422945ff22
Stefano-Paul-Pascu <stefanopascu@gmail.com>
2023-04-08 16:28:08 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import React, { useState, useEffect } from "react";
 
-function App() {
+const WeatherApp = () => {
+  const [weatherData, setWeatherData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const apiKey = "f2c66cfd12c4750e037be67850f32edf";
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setWeatherData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (city) {
+      fetchWeatherData();
+    } else {
+      setWeatherData([]);
+    }
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-primary'>
+      <div className='p-5'>
+      <h1 className='mb-5 text-white'>Previsioni del tempo</h1>
+      <form className='d-flex'>
+        <input
+          className='form-control w-10rem'
+          type="text"
+          value={city}
+          placeholder="Inserisci una città"
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className='btn btn-outline-light ms-3' type="submit">Cerca</button>
+      </form>
+      {isLoading && <p className='text-white'>Caricamento...</p>}
+      {weatherData.length > 0 &&
+        weatherData.list.map((forecast) => (
+          <div key={forecast.dt}>
+            <h2>{forecast.dt_txt}</h2>
+            <p>Temperatura: {forecast.main.temp}°C</p>
+            <p>Min: {forecast.main.temp_min}°C</p>
+            <p>Max: {forecast.main.temp_max}°C</p>
+            <p>Umidità: {forecast.main.humidity}%</p>
+            <p>Pressione: {forecast.main.pressure}hPa</p>
+            <p>Descrizione: {forecast.weather[0].description}</p>
+          </div>
+        ))}
+        </div>
     </div>
   );
-}
+};
 
-export default App;
+export default WeatherApp;
